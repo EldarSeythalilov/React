4049d052902c2843d6004e70f4fdb906e572f223
Эльдар Сейтхалилов <eldarseythalilov@gmail.com>
2024-02-28 02:28:20 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,232 @@
+import React from 'react';
+
+export function App(props) {
+ /*9*/
+	  /*const str1 = "text1";
+	  const str2 = "text2";*/
+	  /*10*/
+	  /*const name = 'user';
+	  const age  = '30';*/
+	  /*11*/
+	  /*const arr = [1, 2, 3, 4, 5];*/
+	  /*12*/
+	  /*const obj = {name: 'john', surname: 'smit'};*/
+    /*13*/
+    /*const attr = 'block';*/
+    /*14*/
+    /*const li1 = <li>text1</li>;
+	  const li2 = <li>text2</li>;
+	  const li3 = <li>text3</li>;*/
+    /*15*/
+    /*const items = <ul><li>text1</li><li>text2</li><li>text3</li></ul>;*/
+    /*19*/
+    /*const num1 = 3;
+	  const num2 = 2;*/
+    /*20*/
+	  /*const name = 'john';
+	  const surname = 'smit';*/
+    /*21*/
+    /*const num = 4;*/
+    /*22*/
+    /*let text;
+    const isAdult = false;
+    if (isAdult) {
+		  text = <p>text1</p>;
+	  } else {
+      text = <p>text2</p>
+    }*/
+    /*23*/
+    /*let text;
+    const isAdmin = true;
+    if (isAdmin) {
+		  text = <p>text</p>
+	  } else {
+
+	  }*/
+    /*24*/
+    /*const age = 19;*/
+	/*25*/
+	/*const isAuth = false;*/
+	/*26*/
+	/*const isAuth = true;*/
+	/*27*/
+	function getDigitsSum(num) {
+		return num == 123;
+	}
+	const sum = getDigitsSum(123);
+	  return (
+		/*1-3*/
+		/*<div className="App">
+		  <h1 class = "aaa">First</h1>
+		  <h2 class = "bbb">Second</h2>
+		  <p>NewOne</p>
+		</div>*/
+	
+		/*4*/
+		/*<ul>
+		  <li>1</li>
+		  <li>2</li>
+		  <li>3</li>
+		  <li>4</li>
+		  <li>5</li>
+		  <li>6</li>
+		  <li>7</li>
+		  <li>8</li>
+		  <li>9</li>
+		  <li>10</li>
+		</ul>*/
+		/*5*/
+		/*<ul>
+				<li>text1</li>
+				<li>text2</li>
+				<li>text3</li>
+			</ul>*/
+		/*6*/
+		/*<>
+		<ul>
+		  <li>text1</li>
+		  <li>text2</li>
+		  <li>text3</li>
+		</ul>
+			<ul>
+			<li>text1</li>
+			<li>text2</li>
+			<li>text3</li>
+		  </ul>
+		  </>
+		  <div>
+		<ul>
+		  <li>text1</li>
+		  <li>text2</li>
+		  <li>text3</li>
+		</ul>
+			<ul>
+			<li>text1</li>
+			<li>text2</li>
+			<li>text3</li>
+		  </ul>
+		  </div>
+		  /*7*/
+		/*<input />*/
+		/*8*/
+		/*<div>
+			<input /><input /><input />
+		  </div>*/
+		/*9*/
+		/*<>
+		  <p>{str1}</p>
+		  <p>{str2}</p>
+		</>*/
+		/*10*/
+		/*<div>
+			<p>name: {name}</p>
+			<p>age:  {age}</p>
+		</div>*/
+		/*11*/
+		/*<div>
+			<ul>
+				<li>{arr[0]}</li>
+				<li>{arr[1]}</li>
+				<li>{arr[2]}</li>
+				<li>{arr[3]}</li>
+				<li>{arr[4]}</li>
+			</ul>
+		</div>*/
+		/*12*/
+		/*<div>
+			<p>
+				name:    <span>{obj.name}</span><br></br>
+				surname: <span>{obj.surname}</span>
+			</p>
+		</div>*/
+    /*13*/
+    /*<div id = {attr}>text</div>*/
+    /*14*/
+    /*<ul>
+	    <li>{li1}</li>
+	    <li>{li2}</li>
+	    <li>{li3}</li>
+    </ul>*/
+	  /*15*/
+    /*<ul>{items}</ul>
+    /*16*/
+    /*<div>
+		  <input /><br></br>
+      <input /><br></br>
+      <input />
+    </div>*/
+    /*17*/
+    /*<div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+      </ul>
+    </div>*/
+    /*18*/
+    /*<div>
+      <table>
+        <thead>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </thead>
+        <tbody>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+        </tbody>
+          <tfoot>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tfoot>
+      </table>
+    </div>*/
+    /*19*/
+    /*<div>
+		  result: {num1 ** num2}
+	  </div>*/
+    /*20*/
+    /*<div>
+		  result: {name + ' ' + surname}
+	  </div>*/
+    /*21*/
+    /*<div>
+		  result: {Math.sqrt(num)}
+	  </div>*/
+    /*22*/
+    /*<div>
+      {text}
+    </div>*/
+    /*23*/
+    /*<div>
+      {text}
+    </div>*/
+	/*24*/
+	/*<div>
+      	{age>18 ? <p>text1</p> : <p>text2</p>}
+    </div>*/
+	/*25*/
+	/*<div>
+		{isAuth && <p>вы авторизованы</p>}
+	</div>*/
+	/*26*/
+	/*<div>
+		{!isAuth && <p>пожалуйста, авторизуйтесь</p>}
+	</div>
+	/*27*/
+	<div>
+		{sum}
+	</div>
+	);
+}
+
+export default App;
